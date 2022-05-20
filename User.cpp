d06b741ef08e237ed1de304b5c56b123070d8b83
Oshada998 <83028294+Oshada998@users.noreply.github.com>
2022-05-20 23:49:14 +0530

    Add files via upload
    added user class

---
+++ User.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+  class User{
+	private:
+
+    int user_id;
+		std ::string userName;
+		std ::string useremail;
+		std ::string userpassword;
+		std ::string userrole;
+		
+	public:
+		void addUser();
+		void editUser();
+		void deleteUser();
+		void getUser();
+};
+
+User::User(int uID,std ::string uname,std ::string uemail,std ::string uemail,std ::string urole, )
+{
+	user_id = uID;
+  userName=  uname;
+	useremail = uemail;
+	userpassword = uemail;
+  userrole =  urole;
+}
+
+void User::addUser(){
+	
+}
+
+void User::editUser(){
+	
+}
+void User::deleteUser(){
+	
+}
+void User::getUser(){
+	
+}
+
+	}
